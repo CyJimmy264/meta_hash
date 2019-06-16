0323b4e2e2188bbab66c3b764bd893f5cbf23b9e
mveynberg <mv@cj264.ru>
2019-06-16 15:55:18 +0500

    Introduce MetaHash::Key

--- lib/meta_hash.rb
+++ lib/meta_hash.rb
@@ -1,6 +1,7 @@
-require "meta_hash/version"
+require 'meta_hash/version'
+require 'meta_hash/key'
+require 'string'
 
 module MetaHash
   class Error < StandardError; end
-  # Your code goes here...
 end
