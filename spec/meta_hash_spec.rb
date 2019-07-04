40c6ebbda84625c6542ae233fba17bf8b4d98277
mveynberg <mv@cj264.ru>
2019-07-04 17:53:51 +0500

    Implement MetaHash.config

--- spec/meta_hash_spec.rb
+++ spec/meta_hash_spec.rb
@@ -1,5 +1,29 @@
 RSpec.describe MetaHash do
-  it "has a version number" do
+  it 'has a version number' do
     expect(MetaHash::VERSION).not_to be nil
+  end
+
+  describe '.config.some_setting in conjunction with .configure' do
+    let(:subject) { described_class.config.some_setting }
+
+    context 'when has not been set' do
+      it { is_expected.to be_nil }
+    end
+
+    context 'when set with "some value"' do
+      before do
+        MetaHash.configure do |c|
+          c.some_setting = 'some value'
+        end
+      end
+
+      after do
+        MetaHash.configure do |c|
+          c.some_setting = nil
+        end
+      end
+
+      it { is_expected.to eq 'some value'}
+    end
   end
 end
